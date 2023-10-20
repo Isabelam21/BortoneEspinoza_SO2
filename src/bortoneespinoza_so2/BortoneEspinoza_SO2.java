e31de0d1ee658bb9e54c20607b57a8600c5b8d31
Isabela Espinoza <isabela.espinoza@correo.unimet.edu.ve>
2023-10-20 13:07:58 -0400

    Inicio

---
+++ src/bortoneespinoza_so2/BortoneEspinoza_SO2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package bortoneespinoza_so2;
+
+/**
+ *
+ * @author isabe
+ */
+public class BortoneEspinoza_SO2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
